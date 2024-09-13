c33ac28481e7364330787e4d13002b93ef88630f
Grigory Taran <grunder-96@mail.ru>
2024-09-13 11:47:49 +0300

    feature: add CurrenciesServlet

---
+++ src/main/java/com/edu/pet/servlet/CurrenciesServlet.java
@@ -0,0 +1,38 @@
+package com.edu.pet.servlet;
+
+import com.edu.pet.dto.CurrencyDto;
+import com.edu.pet.exception.InternalErrorException;
+import com.edu.pet.service.CurrencyService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.modelmapper.ModelMapper;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/currencies")
+public class CurrenciesServlet extends HttpServlet {
+
+    private final CurrencyService currencyService = CurrencyService.getInstance();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ModelMapper modelMapper = new ModelMapper();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        try {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            List<CurrencyDto> currenciesDto = currencyService.findAll();
+            writer.write(objectMapper.writeValueAsString(currenciesDto));
+        } catch (InternalErrorException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        } finally {
+            writer.close();
+        }
+    }
+}
