d9cd18203a064132b0872d0cf05dca1cbad016c9
Grigory Taran <grunder-96@mail.ru>
2024-09-17 11:29:56 +0300

    feature: add doPost in CurrenciesServlet

--- src/main/java/com/edu/pet/servlet/CurrenciesServlet.java
+++ src/main/java/com/edu/pet/servlet/CurrenciesServlet.java
@@ -2,6 +2,7 @@ package com.edu.pet.servlet;
 
 import com.edu.pet.dto.CreateCurrencyDto;
 import com.edu.pet.dto.CurrencyDto;
+import com.edu.pet.exception.CurrencyCodeUniqueException;
 import com.edu.pet.exception.InternalErrorException;
 import com.edu.pet.model.ErrorBody;
 import com.edu.pet.service.CurrencyService;
@@ -33,6 +34,40 @@ public class CurrenciesServlet extends HttpServlet {
             resp.setStatus(SC_OK);
             List<CurrencyDto> currenciesDto = currencyService.findAll();
             objectMapper.writeValue(writer, currenciesDto);
+        } catch (InternalErrorException e) {
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+            objectMapper.writeValue(writer, new ErrorBody(e.getMessage()));
+        } finally {
+            writer.close();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        List<String> missingFields = Arrays.stream(CreateCurrencyDto.class.getDeclaredFields())
+                .map(Field::getName)
+                .filter(Predicate.not(parameterMap::containsKey))
+                .toList();
+        PrintWriter writer = resp.getWriter();
+        try {
+            if (!missingFields.isEmpty()) {
+                resp.setStatus(SC_BAD_REQUEST);
+                StringJoiner joiner = new StringJoiner(", ");
+                missingFields.forEach(joiner::add);
+                objectMapper.writeValue(writer, new ErrorBody("field(-s) not found - " + joiner.toString()));
+            }
+
+            CurrencyDto currencvDto = currencyService.save(new CreateCurrencyDto(
+                    parameterMap.get("code")[0],
+                    parameterMap.get("name")[0],
+                    parameterMap.get("sign")[0]
+            ));
+            resp.setStatus(SC_CREATED);
+            objectMapper.writeValue(writer, currencvDto);
+        } catch (CurrencyCodeUniqueException e) {
+            resp.setStatus(SC_CONFLICT);
+            objectMapper.writeValue(writer, new ErrorBody(e.getMessage()));
         } catch (InternalErrorException e) {
             resp.setStatus(SC_INTERNAL_SERVER_ERROR);
             objectMapper.writeValue(writer, new ErrorBody(e.getMessage()));
