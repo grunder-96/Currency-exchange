4a7202b9a1066405898f3c21092dcf3a033af763
Grigory Taran <grunder-96@mail.ru>
2024-09-17 09:10:59 +0300

    refactor: now CurrenciesServlet use static import for HttpServletResponse, refactor catch-block for InternalError

--- src/main/java/com/edu/pet/servlet/CurrenciesServlet.java
+++ src/main/java/com/edu/pet/servlet/CurrenciesServlet.java
@@ -1,7 +1,9 @@
 package com.edu.pet.servlet;
 
+import com.edu.pet.dto.CreateCurrencyDto;
 import com.edu.pet.dto.CurrencyDto;
 import com.edu.pet.exception.InternalErrorException;
+import com.edu.pet.model.ErrorBody;
 import com.edu.pet.service.CurrencyService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
@@ -9,28 +11,31 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.modelmapper.ModelMapper;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.util.*;
+import java.util.function.Predicate;
+
+import static jakarta.servlet.http.HttpServletResponse.*;
 
 @WebServlet("/currencies")
 public class CurrenciesServlet extends HttpServlet {
 
     private final CurrencyService currencyService = CurrencyService.getInstance();
     private final ObjectMapper objectMapper = new ObjectMapper();
-    private final ModelMapper modelMapper = new ModelMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         try {
-            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setStatus(SC_OK);
             List<CurrencyDto> currenciesDto = currencyService.findAll();
             objectMapper.writeValue(writer, currenciesDto);
         } catch (InternalErrorException e) {
-            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+            objectMapper.writeValue(writer, new ErrorBody(e.getMessage()));
         } finally {
             writer.close();
         }
