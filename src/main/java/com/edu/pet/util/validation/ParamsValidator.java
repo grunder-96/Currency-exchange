676c78a909d64abaf1714e8b1ccc79ea6af0bc8a
Grigory Taran <grunder-96@mail.ru>
2024-09-19 16:58:38 +0300

    feature: add missing and empty parameters validator

---
+++ src/main/java/com/edu/pet/util/validation/ParamsValidator.java
@@ -0,0 +1,27 @@
+package com.edu.pet.util.validation;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ParamsValidator {
+
+    private final List<String> requiredParams;
+    private final HttpServletRequest req;
+
+    public ParamsValidator(HttpServletRequest req, List<String> requiredParams) {
+        this.req = Objects.requireNonNull(req, "request is null");
+        this.requiredParams = Objects.requireNonNull(requiredParams, "required parameters is null");
+    }
+
+    public boolean isValid() {
+        return getInvalidParams().isEmpty();
+    }
+
+    public List<String> getInvalidParams() {
+        return requiredParams.stream()
+                .filter(param -> req.getParameter(param).isEmpty())
+                .toList();
+    }
+}
