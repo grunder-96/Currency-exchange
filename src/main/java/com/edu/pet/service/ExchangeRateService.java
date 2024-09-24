5b66f65d726aab484cb4a1c285634c3250874b0c
Grigory Taran <grunder-96@mail.ru>
2024-09-24 16:26:03 +0300

    chore: remove commented code in ExchangeRateService

--- src/main/java/com/edu/pet/service/ExchangeRateService.java
+++ src/main/java/com/edu/pet/service/ExchangeRateService.java
@@ -2,10 +2,13 @@ package com.edu.pet.service;
 
 import com.edu.pet.dao.CurrencyDao;
 import com.edu.pet.dao.ExchangeRateDao;
+import com.edu.pet.dto.CreateCurrencyDto;
 import com.edu.pet.dto.ExchangeRateDto;
+import com.edu.pet.exception.InternalErrorException;
 import com.edu.pet.model.ExchangeRate;
 import org.modelmapper.ModelMapper;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,45 +16,27 @@ public class ExchangeRateService {
 
     private static final ExchangeRateService INSTANCE = new ExchangeRateService();
     private final ExchangeRateDao exchangeRateDao = ExchangeRateDao.getInstance();
-    private final CurrencyDao currencyDao = CurrencyDao.getInstance();
     private final ModelMapper modelMapper = new ModelMapper();
 
     private ExchangeRateService() {
 
     }
 
-//    {
-//        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-//        modelMapper.createTypeMap(ExchangeRate.class, ExchangeRateDto.class).addMappings(mapper -> {
-//            mapper.skip(ExchangeRateDto::setBaseCurrency);
-//            mapper.skip(ExchangeRateDto::setTargetCurrency);
-//        }).setPostConverter(toExchangeRateDto());
-//    }
-
-
-    public List<ExchangeRateDto> findAll() {
+    public List<ExchangeRateDto> findAll() throws InternalErrorException {
         List<ExchangeRate> exchangeRates = exchangeRateDao.findAll();
         return exchangeRates.stream()
                 .map(exchangeRate -> modelMapper.map(exchangeRate, ExchangeRateDto.class))
                 .toList();
     }
 
-    public Optional<ExchangeRateDto> findByCodePair(String baseCurrencyCode, String targetCurrencyCode) {
+    public Optional<ExchangeRateDto> findByCodePair(String baseCurrencyCode, String targetCurrencyCode) throws InternalErrorException {
         Optional<ExchangeRate> maybeExchangeRate = exchangeRateDao.findByCodePair(baseCurrencyCode, targetCurrencyCode);
         return maybeExchangeRate.map(exchangeRate -> modelMapper.map(maybeExchangeRate, ExchangeRateDto.class));
     }
 
-//    private Converter<ExchangeRate, ExchangeRateDto> toExchangeRateDto() {
-//        return context -> {
-//            ExchangeRate source = context.getSource();
-//            ExchangeRateDto destination = context.getDestination();
-//
-//            destination.setBaseCurrency(modelMapper.map(currencyDao.findById(source.getBaseCurrency()).get(), CurrencyDto.class));
-//            destination.setTargetCurrency(modelMapper.map(currencyDao.findById(source.getTargetCurrency()).get(), CurrencyDto.class));
-//
-//            return context.getDestination();
-//        };
-//    }
+    public ExchangeRateDto save(String baseCurrencyCode, String targetCurrencyCode, BigDecimal rate) throws InternalErrorException {
+        return modelMapper.map(exchangeRateDao.save(baseCurrencyCode, targetCurrencyCode, rate), ExchangeRateDto.class);
+    }
 
     public static ExchangeRateService getInstance() {
         return INSTANCE;
