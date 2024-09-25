0e08545b7a2f99280c2be13bfd51af67fee36357
Grigory Taran <grunder-96@mail.ru>
2024-09-25 11:02:48 +0300

    feature: add CreateUpdateRateDto

---
+++ src/main/java/com/edu/pet/dto/CreateUpdateRateDto.java
@@ -0,0 +1,44 @@
+package com.edu.pet.dto;
+
+import java.math.BigDecimal;
+
+public class CreateUpdateRateDto {
+
+    private String baseCurrencyCode;
+    private String targetCurrencyCode;
+    private BigDecimal rate;
+
+    public CreateUpdateRateDto() {
+
+    }
+
+    public CreateUpdateRateDto(String baseCurrencyCode, String targetCurrencyCode, BigDecimal rate) {
+        this.baseCurrencyCode = baseCurrencyCode;
+        this.targetCurrencyCode = targetCurrencyCode;
+        this.rate = rate;
+    }
+
+    public String getBaseCurrencyCode() {
+        return baseCurrencyCode;
+    }
+
+    public void setBaseCurrencyCode(String baseCurrencyCode) {
+        this.baseCurrencyCode = baseCurrencyCode;
+    }
+
+    public String getTargetCurrencyCode() {
+        return targetCurrencyCode;
+    }
+
+    public void setTargetCurrencyCode(String targetCurrencyCode) {
+        this.targetCurrencyCode = targetCurrencyCode;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+}
