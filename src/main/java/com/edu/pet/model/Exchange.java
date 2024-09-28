bf5187db42e8f1e8a3f1d8866c0f0be50c6304c9
Grigory Taran <grunder-96@mail.ru>
2024-09-28 10:16:34 +0300

    feature: add Exchange in model

---
+++ src/main/java/com/edu/pet/model/Exchange.java
@@ -0,0 +1,65 @@
+package com.edu.pet.model;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Exchange {
+
+    private Currency baseCurrency;
+    private Currency targetCurrency;
+    private BigDecimal rate;
+    private BigDecimal amount;
+    private BigDecimal convertedAmount;
+
+    public Exchange() {
+
+    }
+
+    public Exchange(Currency baseCurrency, Currency targetCurrency, BigDecimal rate, BigDecimal amount, BigDecimal convertedAmount) {
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+        this.amount = amount;
+        this.convertedAmount = convertedAmount;
+    }
+
+    public Currency getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(Currency baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public Currency getTargetCurrency() {
+        return targetCurrency;
+    }
+
+    public void setTargetCurrency(Currency targetCurrency) {
+        this.targetCurrency = targetCurrency;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    public BigDecimal getAmount() {
+        return amount.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public void setConvertedAmount(BigDecimal convertedAmount) {
+        this.convertedAmount = convertedAmount;
+    }
+}
