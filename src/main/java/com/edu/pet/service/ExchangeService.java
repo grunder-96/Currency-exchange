faaedf714d783c99e66bc0f22e4e31fc772c806d
Grigory Taran <grunder-96@mail.ru>
2024-09-28 16:29:07 +0300

    feature: implements service and servlet layers for exchange

---
+++ src/main/java/com/edu/pet/service/ExchangeService.java
@@ -0,0 +1,93 @@
+package com.edu.pet.service;
+
+import com.edu.pet.dao.ExchangeRateDao;
+import com.edu.pet.exception.InternalErrorException;
+import com.edu.pet.exception.NonExistsException;
+import com.edu.pet.model.Currency;
+import com.edu.pet.model.Exchange;
+import com.edu.pet.model.ExchangeRate;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.Optional;
+
+public class ExchangeService {
+
+    private static final ExchangeService INSTANCE = new ExchangeService();
+    private final ExchangeRateDao exchangeRateDao = ExchangeRateDao.getInstance();
+
+    private ExchangeService() {
+
+    }
+
+    public Exchange exchange(String baseCurrencyCode, String targetCurrencyCode, BigDecimal amount) throws NonExistsException, InternalErrorException {
+        Optional<ExchangeRate> maybeExchangeRate = exchangeRateDao.findByCodePair(baseCurrencyCode, targetCurrencyCode);
+
+        if (maybeExchangeRate.isPresent()) {
+            return exchangeByDirectRate(maybeExchangeRate.get(), amount);
+        }
+
+        Optional<ExchangeRate> maybeReverseRate = exchangeRateDao.findByCodePair(targetCurrencyCode, baseCurrencyCode);
+
+        if (maybeReverseRate.isPresent()) {
+            return exchangeByReverseRate(maybeReverseRate.get(), amount);
+        }
+
+        List<ExchangeRate> crossExchangeList = exchangeRateDao.findByCrossRate(baseCurrencyCode, targetCurrencyCode);
+
+        if (crossExchangeList.size() == 2) {
+            if (crossExchangeList.getFirst().getTargetCurrency().getCode().equalsIgnoreCase(targetCurrencyCode)) {
+                crossExchangeList = crossExchangeList.reversed();
+            }
+            return exchangeByCrossRate(crossExchangeList, amount);
+        }
+
+        throw new NonExistsException("no exchange method found for this currency pair");
+    }
+
+    private Exchange exchangeByDirectRate(ExchangeRate exchangeRate, BigDecimal amount) {
+        BigDecimal convertedAmount = exchangeRate.getRate().multiply(amount);
+        return buildExchange(exchangeRate, amount, convertedAmount);
+    }
+
+    private Exchange exchangeByReverseRate(ExchangeRate exchangeRate, BigDecimal amount) {
+        Currency baseCurrency = exchangeRate.getBaseCurrency();
+        exchangeRate.setBaseCurrency(exchangeRate.getTargetCurrency());
+        exchangeRate.setTargetCurrency(baseCurrency);
+
+        BigDecimal rate = BigDecimal.ONE.divide(exchangeRate.getRate(), 6, RoundingMode.HALF_DOWN);
+        BigDecimal convertedAmount = rate.multiply(amount);
+
+        exchangeRate.setRate(rate);
+
+        return buildExchange(exchangeRate, amount, convertedAmount);
+    }
+
+    private Exchange exchangeByCrossRate(List<ExchangeRate> crossExchangeList, BigDecimal amount) {
+        ExchangeRate toBaseExchangeRate = crossExchangeList.getFirst();
+        ExchangeRate toTargetExchangeRate = crossExchangeList.getLast();
+
+        BigDecimal rate = toTargetExchangeRate.getRate().divide(toBaseExchangeRate.getRate(), 6, RoundingMode.HALF_DOWN);
+        BigDecimal convertedAmount = rate.multiply(amount);
+
+        toTargetExchangeRate.setRate(rate);
+        toTargetExchangeRate.setBaseCurrency(toBaseExchangeRate.getTargetCurrency());
+
+        return buildExchange(toTargetExchangeRate, amount, convertedAmount);
+    }
+
+    private Exchange buildExchange(ExchangeRate exchangeRate, BigDecimal amount, BigDecimal convertedAmount) {
+        return new Exchange(
+              exchangeRate.getBaseCurrency(),
+              exchangeRate.getTargetCurrency(),
+              exchangeRate.getRate(),
+              amount,
+              convertedAmount
+        );
+    }
+
+    public static ExchangeService getInstance() {
+        return INSTANCE;
+    }
+}
---
+++ src/main/java/com/edu/pet/servlet/ExchangeServlet.java
@@ -0,0 +1,79 @@
+package com.edu.pet.servlet;
+
+import com.edu.pet.exception.InternalErrorException;
+import com.edu.pet.exception.NonExistsException;
+import com.edu.pet.model.ErrorBody;
+import com.edu.pet.service.ExchangeService;
+import com.edu.pet.util.validation.CurrencyCodeValidator;
+import com.edu.pet.util.validation.ParamsValidator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.List;
+
+import static jakarta.servlet.http.HttpServletResponse.*;
+import static jakarta.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+
+@WebServlet("/exchange")
+public class ExchangeServlet extends HttpServlet {
+
+    private final ExchangeService exchangeService = ExchangeService.getInstance();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        ParamsValidator paramsValidator = new ParamsValidator(req, List.of("from", "to", "amount"));
+        try {
+            if (!paramsValidator.isValid()) {
+                resp.setStatus(SC_BAD_REQUEST);
+                objectMapper.writeValue(writer, new ErrorBody("parameter(-s) not found or empty - %s".formatted(
+                        String.join(", ", paramsValidator.getInvalidParams())
+                )));
+                return;
+            }
+
+            String baseCurrencyCode = req.getParameter("from").trim();
+            String targetCurrencyCode = req.getParameter("to").trim();
+
+            if (!(CurrencyCodeValidator.isValid(baseCurrencyCode)
+                  || CurrencyCodeValidator.isValid(targetCurrencyCode))) {
+                resp.setStatus(SC_BAD_REQUEST);
+                objectMapper.writeValue(writer, new ErrorBody("one or both currency codes are not valid"));
+                return;
+            }
+
+            BigDecimal amount;
+            try {
+                amount = new BigDecimal(req.getParameter("amount").trim());
+                if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+                    throw new IllegalArgumentException("amount must be greater than zero");
+                }
+            } catch (RuntimeException e) {
+                resp.setStatus(SC_BAD_REQUEST);
+                objectMapper.writeValue(writer,
+                        new ErrorBody(e.getClass().equals(IllegalArgumentException.class) ?
+                                e.getMessage() : "amount is not valid"));
+                return;
+            }
+
+            resp.setStatus(SC_OK);
+            objectMapper.writeValue(writer, exchangeService.exchange(baseCurrencyCode, targetCurrencyCode, amount));
+        } catch (NonExistsException e) {
+            resp.setStatus(SC_NOT_FOUND);
+            objectMapper.writeValue(writer, new ErrorBody(e.getMessage()));
+        } catch (InternalErrorException e) {
+            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
+            objectMapper.writeValue(writer, new ErrorBody(e.getMessage()));
+        } finally {
+            writer.close();
+        }
+    }
+}
