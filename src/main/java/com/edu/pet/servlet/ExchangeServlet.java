4052c73f8a0ba6aa5def16ed7de1f0cea38f6d71
Grigory Taran <grunder-96@mail.ru>
2024-10-02 13:16:04 +0300

    refactor: now ExchangeServlet uses ResponseWrapper

--- src/main/java/com/edu/pet/servlet/ExchangeServlet.java
+++ src/main/java/com/edu/pet/servlet/ExchangeServlet.java
@@ -2,11 +2,9 @@ package com.edu.pet.servlet;
 
 import com.edu.pet.exception.InternalErrorException;
 import com.edu.pet.exception.NonExistsException;
-import com.edu.pet.model.ErrorBody;
 import com.edu.pet.service.ExchangeService;
-import com.edu.pet.util.validation.CurrencyCodeValidator;
-import com.edu.pet.util.validation.ParamsValidator;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.edu.pet.util.ResponseWrapper;
+import com.edu.pet.util.validation.CurrencyPairValidator;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,7 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -25,32 +22,22 @@ import static jakarta.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 public class ExchangeServlet extends HttpServlet {
 
     private final ExchangeService exchangeService = ExchangeService.getInstance();
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        ParamsValidator paramsValidator = new ParamsValidator(req, List.of("from", "to", "amount"));
-        try {
-            if (!paramsValidator.isValid()) {
-                resp.setStatus(SC_BAD_REQUEST);
-                objectMapper.writeValue(writer, new ErrorBody("parameter(-s) not found or empty - %s".formatted(
-                        String.join(", ", paramsValidator.getInvalidParams())
-                )));
-                return;
-            }
+        EmptyParamsValidator emptyParamsValidator = new EmptyParamsValidator(req, List.of("from", "to", "amount"));
+        if (!emptyParamsValidator.isValid()) {
+            ResponseWrapper.configureErrorResponse(resp, SC_BAD_REQUEST, emptyParamsValidator.getInvalidParamsAsString());
+            return;
+        }
 
-            String baseCurrencyCode = req.getParameter("from").trim();
-            String targetCurrencyCode = req.getParameter("to").trim();
+        String baseCurrencyCode = req.getParameter("from").trim();
+        String targetCurrencyCode = req.getParameter("to").trim();
 
-            if (!(
-                    CurrencyCodeValidator.isValid(baseCurrencyCode) &&
-                    CurrencyCodeValidator.isValid(targetCurrencyCode)
-                )) {
-                resp.setStatus(SC_BAD_REQUEST);
-                objectMapper.writeValue(writer, new ErrorBody("one or both currency codes are not valid"));
-                return;
-            }
+        if (!CurrencyPairValidator.isValid(baseCurrencyCode, targetCurrencyCode)) {
+            ResponseWrapper.configureErrorResponse(resp, SC_BAD_REQUEST, "one or both currency codes are not valid");
+            return;
+        }
 
             BigDecimal amount;
             try {
@@ -59,23 +46,17 @@ public class ExchangeServlet extends HttpServlet {
                     throw new IllegalArgumentException("amount must be greater than zero");
                 }
             } catch (RuntimeException e) {
-                resp.setStatus(SC_BAD_REQUEST);
-                objectMapper.writeValue(writer,
-                        new ErrorBody(e.getClass().equals(IllegalArgumentException.class) ?
-                                e.getMessage() : "amount is not valid"));
+                ResponseWrapper.configureErrorResponse(resp, SC_BAD_REQUEST, e.getClass().equals(IllegalArgumentException.class) ?
+                        e.getMessage() : "amount is not valid");
                 return;
             }
 
-            resp.setStatus(SC_OK);
-            objectMapper.writeValue(writer, exchangeService.exchange(baseCurrencyCode, targetCurrencyCode, amount));
+        try {
+            ResponseWrapper.configureResponse(resp, SC_OK, exchangeService.exchange(baseCurrencyCode, targetCurrencyCode, amount));
         } catch (NonExistsException e) {
-            resp.setStatus(SC_NOT_FOUND);
-            objectMapper.writeValue(writer, new ErrorBody(e.getMessage()));
+            ResponseWrapper.configureErrorResponse(resp, SC_NOT_FOUND, e);
         } catch (InternalErrorException e) {
-            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
-            objectMapper.writeValue(writer, new ErrorBody(e.getMessage()));
-        } finally {
-            writer.close();
+            ResponseWrapper.configureErrorResponse(resp, SC_INTERNAL_SERVER_ERROR, e);
         }
     }
 }
