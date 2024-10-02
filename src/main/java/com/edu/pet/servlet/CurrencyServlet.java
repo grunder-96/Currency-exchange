da1c426b0fffc291a6301632f656439513c1691c
Grigory Taran <grunder-96@mail.ru>
2024-10-02 11:36:42 +0300

    refactor: now CurrencyServlet uses ResponseWrapper

--- src/main/java/com/edu/pet/servlet/CurrencyServlet.java
+++ src/main/java/com/edu/pet/servlet/CurrencyServlet.java
@@ -2,7 +2,7 @@ package com.edu.pet.servlet;
 
 import com.edu.pet.dto.CurrencyDto;
 import com.edu.pet.exception.InternalErrorException;
-import com.edu.pet.model.ErrorBody;
+import com.edu.pet.util.ResponseWrapper;
 import com.edu.pet.service.CurrencyService;
 import com.edu.pet.util.validation.CurrencyCodeValidator;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,7 +13,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Optional;
 
 import static jakarta.servlet.http.HttpServletResponse.*;
@@ -22,42 +21,35 @@ import static jakarta.servlet.http.HttpServletResponse.*;
 public class CurrencyServlet extends HttpServlet {
 
     private final CurrencyService currencyService = CurrencyService.getInstance();
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
+
         Optional<String> maybeCode = Optional.ofNullable(req.getPathInfo());
+
+        if (maybeCode.isEmpty()) {
+            ResponseWrapper.configureErrorResponse(resp, SC_BAD_REQUEST, "currency code missing in url");
+            return;
+        }
+
+        String code = maybeCode.get().replace("/", "");
+
+        if (!CurrencyCodeValidator.isValid(code)) {
+            ResponseWrapper.configureErrorResponse(resp, SC_BAD_REQUEST, "currency code is invalid");
+            return;
+        }
+
         try {
-            if (maybeCode.isEmpty()) {
-                resp.setStatus(SC_BAD_REQUEST);
-                objectMapper.writeValue(writer, new ErrorBody("currency code missing in url"));
-                return;
-            }
-
-            String code = maybeCode.get().replace("/", "");
-
-            if (!CurrencyCodeValidator.isValid(code)) {
-                resp.setStatus(SC_BAD_REQUEST);
-                objectMapper.writeValue(writer, new ErrorBody("currency code is invalid"));
-                return;
-            }
-
             Optional<CurrencyDto> maybeCurrencyDto = currencyService.findByCode(code);
 
             if (maybeCurrencyDto.isEmpty()) {
-                resp.setStatus(SC_NOT_FOUND);
-                objectMapper.writeValue(writer, new ErrorBody("such currency is not in the database"));
+                ResponseWrapper.configureErrorResponse(resp, SC_NOT_FOUND, "such currency is not in the database");
                 return;
             }
 
-            resp.setStatus(SC_OK);
-            objectMapper.writeValue(writer, maybeCurrencyDto.get());
+            ResponseWrapper.configureResponse(resp, SC_OK, maybeCurrencyDto.get());
         } catch (InternalErrorException e) {
-            resp.setStatus(SC_INTERNAL_SERVER_ERROR);
-            objectMapper.writeValue(writer, new ErrorBody(e.getMessage()));
-        } finally {
-            writer.close();
+            ResponseWrapper.configureErrorResponse(resp, SC_INTERNAL_SERVER_ERROR, e);
         }
     }
 }
